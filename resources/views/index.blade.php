aa65a5a21d1f1c23443c8818dbaaf577c76bb7e6
Manoel Musskopf <manoel.240100@bento.local>
2022-05-12 20:03:22 -0300

    Criada pagina inicial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('templates/layout')
+
+@section('titulo', 'Página Principal')
+
+@section('body')
+
+    <h1>Página Principal</h1>
+
+    <p>Teste</p>
+
+    
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/produtos/index', [ProdutoController::class, 'index'])->name('produtos/index');
